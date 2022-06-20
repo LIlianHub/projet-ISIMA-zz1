f318408d841a52dce7b0bb472c5e040f66fd893f
Xethail <xethail@xana.code_lyoko>
2022-06-21 01:48:52 +0200

    LEGER : fenetreX fini

--- ProjetsIndiv/FenetreX/LegerBertrand/sources/main.c
+++ ProjetsIndiv/FenetreX/LegerBertrand/sources/main.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <math.h>
 
-#define NBREWINDOWS 100
+#define NBREWINDOWS 400
+
 
 
 int main(int argc, char **argv) {
@@ -13,7 +14,7 @@ int main(int argc, char **argv) {
   (void)argc;
   (void)argv;
 
-  SDL_Window *windows [NBREWINDOWS*2];
+  SDL_Window *windows [NBREWINDOWS];
 
 
   
@@ -31,44 +32,101 @@ int main(int argc, char **argv) {
   int height = current.h;
 
 
-  float position_x=0;
-  float position_y=0;
+  float position_x_left=0; //première fenêtre côté gauche au milieu de la largeur de l'écran
+  float position_y_left=height/2;
 
-  while(position_x < height/2){
+  float position_x_right=width; //première fenêtre côté droit au milieu de la largeur de l'écran
+  float position_y_right=height/2;
+
+  
+  /*Animation 1:
+   *ligne de fenêtre de la moitié de la largeur de l'écran et de longueur = 1/3 de l'écran
+   *Qui se referme une fois que la dernière est placée*/
+  
+  while(position_x_left < width/2 && position_x_right > width/2 ){
 
     windows[i] = SDL_CreateWindow(
-				  "left circle",
-				  0,0,
-				  70,70,
+				  "left",
+				  position_x_left,position_y_left,
+				  40,20,
 				  0
 				  );
 
-    if(windows[i] == NULL){
-      SDL_Log("Erro : SDL windox %d creation - %s\n",i,SDL_GetError());    
+    windows[200+i] = SDL_CreateWindow(
+				      "right",
+				      position_x_right,position_y_right,
+				      40,20,
+				      SDL_WINDOW_RESIZABLE
+				      );
+    
+    if(windows[i] == NULL || windows[200+i] == NULL){
+      SDL_Log("Error : SDL window %d creation - %s\n",i,SDL_GetError());    
       SDL_Quit();
       exit(EXIT_FAILURE);
     }
-    position_x = position_x + 15;
-    position_y = position_y + 15;
-    SDL_SetWindowPosition(windows[i],position_x,position_y);
+    position_x_left = position_x_left + 15;
+    SDL_SetWindowPosition(windows[i],position_x_left,position_y_left);
 
-    SDL_Delay(75);
+    position_x_right = position_x_right - 15;
+    SDL_SetWindowPosition(windows[i],position_x_left,position_y_left);
+    
+    SDL_Delay(25);
     i++;
   }
   
   SDL_Delay(300);
-  for (j = 0 ; j < i-1 ; j++){
+  
+  for (j = 0 ; j < i ; j++){
     
     SDL_DestroyWindow(windows[j]);
-    SDL_Delay(200);  
+    if(j<i-1){
+      SDL_DestroyWindow(windows[200+j]);
+    }
+    SDL_Delay(75);  
   }
 
+  /*Animation 2 :
+   *Je fais tourner fenêtres grâce aux coordonnées du cercle trigonométrique
+   *A chaque itération le rayon du cercle va grandir*/
 
-  SDL_SetWindowPosition(windows[j+1],cosf(position_x),sinf(position_y));
+  int b;
   
+  for ( b = 0 ; b <= 100 ; b++ ){ 
+
+    windows[b] = SDL_CreateWindow(
+				  "left circle",
+				  position_x_left,position_y_left,
+				  40,30,
+				  0
+				  );
+
+    if(windows[b] == NULL){
+      SDL_Log("Error : SDL window %d creation - %s\n",b,SDL_GetError());    
+      SDL_Quit();
+      exit(EXIT_FAILURE);
+    }
+
+
+    float pos_x = cosf(b)*25*(b/4) + position_x_left;
+    float pos_y = sinf(b)*25*(b/4) + position_y_left;
+    
+    SDL_SetWindowPosition(windows[b],pos_x,pos_y);
+    SDL_Delay(75);
+  }
+
+  SDL_Delay(3000);
+
+  for (b = 100 ; b>=0; b--){
+    
+    SDL_DestroyWindow(windows[b]);
+    SDL_Delay(10);
+    
+  }
+  SDL_DestroyWindow(windows[1]);
+
   SDL_Delay(5000);
   
-  SDL_Quit();                                // quitte la SDL
+  SDL_Quit();            // quitte la SDL
 
   return 0;
 }
