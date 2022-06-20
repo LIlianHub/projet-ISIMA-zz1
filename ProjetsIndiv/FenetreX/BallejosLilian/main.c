74df6d45b5a768c59997f9246d322603e2317b84
LILianHUb <lilian.ballejos@hotmail.fr>
2022-06-20 14:47:45 +0200

    fonctionne

--- ProjetsIndiv/FenetreX/BallejosLilian/main.c
+++ ProjetsIndiv/FenetreX/BallejosLilian/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define TAILLE_FENETRE 100
+#define TAILLE_FENETRE 25
 
 int main(int argc, char **argv)
 {
@@ -27,36 +27,54 @@ int main(int argc, char **argv)
   int milieu = height / 2 - TAILLE_FENETRE;
   int nbr_element = width / TAILLE_FENETRE;
 
-
   /*Fenetre*/
 
-  SDL_Window * tabWindow[nbr_element];
+  SDL_Window *tabWindow[nbr_element];
   for (int i = 0; i < nbr_element; i++)
   {
     tabWindow[i] = SDL_CreateWindow(
-        "Fenetre",             // codage en utf8, donc accents possibles
-        0 + i * TAILLE_FENETRE, milieu,     // coin haut gauche en haut gauche de l'écran
-        TAILLE_FENETRE, TAILLE_FENETRE,        
+        "Fenetre",                  // codage en utf8, donc accents possibles
+        i * TAILLE_FENETRE, milieu, // coin haut gauche en haut gauche de l'écran
+        TAILLE_FENETRE, TAILLE_FENETRE,
         0); // redimensionnable
 
     if (tabWindow[i] == NULL)
     {
       SDL_Log("Error : SDL window %d creation - %s\n",
-              i,SDL_GetError()); // échec de la création de la fenêtre
-      SDL_Quit();              // On referme la SDL
+              i, SDL_GetError()); // échec de la création de la fenêtre
+      SDL_Quit();                 // On referme la SDL
       exit(EXIT_FAILURE);
     }
   }
 
-  /*Animation*/
-  int iteration = 1000;
-  while(iteration > 0){
+  /*ANimation 1*/
+
+  for (int i = 0; i < nbr_element; i++)
+  {
+    float val_cos = cosf((i) * TAILLE_FENETRE);
+    SDL_SetWindowPosition(tabWindow[i], i * TAILLE_FENETRE, val_cos * val_cos * height);
+    SDL_Delay(100);
+  }
+
+  /*Animation 2*/
+  int variante = 0;
+  while (variante < 100)
+  {
     for (int i = 0; i < nbr_element; i++)
     {
-      SDL_SetWindowPosition(tabWindow[i], milieu, cos(i) * 10);
+      float val_cos = cosf((i + variante) * TAILLE_FENETRE);
+      SDL_SetWindowPosition(tabWindow[i], i * TAILLE_FENETRE, val_cos * val_cos * height);
     }
-    iteration--;
-    printf("interation : %d\n", iteration);
+    variante++;
+    printf("iteration n°%d\n", variante);
+    SDL_Delay(100);
+  }
+
+  /*ANimation 3*/
+
+  for (int i = nbr_element - 1; i >= 0; i--)
+  {
+    SDL_SetWindowPosition(tabWindow[i], i * TAILLE_FENETRE, milieu);
     SDL_Delay(100);
   }
 
