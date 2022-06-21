1ef2db1d9f02da9feca0439f61c3d203703d1092
LILianHUb <lilian.ballejos@hotmail.fr>
2022-06-21 09:47:46 +0200

    mieux

--- ProjetsIndiv/Frame/BallejosLilian/main.c
+++ ProjetsIndiv/Frame/BallejosLilian/main.c
@@ -10,7 +10,7 @@ void end_sdl(char ok,            // fin normale : ok = 0 ; anormale ok = 1
              SDL_Renderer *renderer,
              SDL_Texture *sol,
              SDL_Texture *ciel,
-             SDL_Texture *mario)
+             SDL_Texture *oiseau)
 { // renderer à fermer
     char msg_formated[255];
     int l;
@@ -47,10 +47,10 @@ void end_sdl(char ok,            // fin normale : ok = 0 ; anormale ok = 1
         ciel = NULL;
     }
 
-    if (mario != NULL)
-    {                              // Destruction si nécessaire de la fenêtre
-        SDL_DestroyTexture(mario); // Attention : on suppose que les NULL sont maintenus !!
-        mario = NULL;
+    if (oiseau != NULL)
+    {                               // Destruction si nécessaire de la fenêtre
+        SDL_DestroyTexture(oiseau); // Attention : on suppose que les NULL sont maintenus !!
+        oiseau = NULL;
     }
 
     SDL_Quit();
@@ -88,7 +88,7 @@ void anim(SDL_Texture *ciel,
           SDL_Window *window,
           SDL_Renderer *renderer,
           SDL_Texture *sol,
-          SDL_Texture *mario)
+          SDL_Texture *oiseau)
 {
     /* perso*/
     SDL_Rect
@@ -101,22 +101,32 @@ void anim(SDL_Texture *ciel,
                       &window_dimensions2.w,
                       &window_dimensions2.h);
 
-    SDL_QueryTexture(mario, // Récupération des dimensions de l'image
+    SDL_QueryTexture(oiseau, // Récupération des dimensions de l'image
                      NULL, NULL,
                      &source2.w, &source2.h);
 
-    int nb_images = 3;                        // Il y a 3 vignette dans la ligne de l'image qui nous intéresse
-    int offset_x = source2.w / 6,             // 6 frame par ligne
-        offset_y = source2.h / 4 + source2.h; // on se place sur la deuxieme ligne de l'image
+    printf("dim image %d %d\n", source2.w, source2.h);
 
-    float zoom2 = 1;                                             // zoom, car ces images sont un peu petites
-    destination2.w = offset_x * zoom2;                           // Largeur du sprite à l'écran
-    destination2.h = offset_y * zoom2;                           // Hauteur du sprite à l'écran
-    destination2.y = (window_dimensions2.h - destination2.h) / 2; // La course se fait en milieu d'écran (en vertical)
+    int nb_images = 5;
+    int offset_x = source2.w / nb_images, // 5 frame par ligne
+        offset_y = source2.h / 3;         // 3 lignes
 
-    printf("-->%d %d\n", destination2.w, destination2.h);
 
-    printf("%d %d\n", offset_x, offset_y);
+    state2.x = 0;
+    state2.y = 0; // premiere ligne
+    state2.w = offset_x;
+    state2.h = offset_y;
+
+
+
+    printf("offeset %d %d\n", offset_x, offset_y);
+
+    float zoom2 = 1;                                              // zoom, car ces images sont un peu petites
+    destination2.w = offset_x * zoom2;                            // Largeur du sprite à l'écran
+    destination2.h = offset_y * zoom2;                            // Hauteur du sprite à l'écran
+    destination2.y = (window_dimensions2.h / 2) - destination2.h; // La course se fait en milieu d'écran (en vertical)
+
+    printf("destination H et W %d %d\n", destination2.w, destination2.h);
 
     /* fond */
     SDL_Rect
@@ -137,7 +147,6 @@ void anim(SDL_Texture *ciel,
     destination.h = source.h * zoom; // On applique le zoom sur la hauteur
     destination.x = -source.w / 2;   // au depart a droite
 
-
     /*animation*/
     int speed = 4; // vitesse de déplacement
     for (int x = 0; x < (window_dimensions.w - destination2.w); x += speed)
@@ -151,18 +160,18 @@ void anim(SDL_Texture *ciel,
             destination.x += 10;
         }
         destination2.x = x;
-        state2.x += offset_x;                    // On passe à la vignette suivante dans l'image
-        state2.x %= (source2.w / 6) * nb_images; // La vignette suivante sur 3 cases
-        state2.y = offset_y;
+        state2.x += offset_x;  // On passe à la vignette suivante dans l'image
+        state2.x %= source2.w; // On réinitialise à 0 si on dépasse la largeur
 
-        //printf("%d %d\n", state2.x, state2.y);
+        printf("etat %d %d\n", state2.x, state2.y);
+        printf("destination %d %d\n", destination2.x, destination2.y);
 
-        SDL_RenderClear(renderer);                                   // Effacer l'image précédente
+        SDL_RenderClear(renderer); // Effacer l'image précédente
         SDL_RenderCopy(renderer, ciel, &source, &destination); // Préparation de l'affichage
         placementTexture(sol, window, renderer);                     // Afficher le sol
-        SDL_RenderCopy(renderer, mario, &state2, &destination2);     // personnage
-        SDL_RenderPresent(renderer);                                 // Affichage de la nouvelle image
-        SDL_Delay(30);                                               // Pause en ms
+        SDL_RenderCopy(renderer, oiseau, &state2, &destination2); // personnage
+        SDL_RenderPresent(renderer);                              // Affichage de la nouvelle image
+        SDL_Delay(30);                                            // Pause en ms
     }
 }
 
@@ -175,13 +184,13 @@ int main(int argc, char **argv)
     SDL_DisplayMode screen;
     SDL_Texture *sol = NULL;
     SDL_Texture *ciel = NULL;
-    SDL_Texture *mario = NULL;
+    SDL_Texture *oiseau = NULL;
     SDL_Renderer *renderer = NULL;
 
     /*********************************************************************************************************************/
     /*                         Initialisation de la SDL  + gestion de l'échec possible                                   */
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        end_sdl(0, "ERROR SDL INIT", window, renderer, sol, ciel, mario);
+        end_sdl(0, "ERROR SDL INIT", window, renderer, sol, ciel, oiseau);
 
     SDL_GetCurrentDisplayMode(0, &screen);
     printf("Résolution écran\n\tw : %d\n\th : %d\n",
@@ -194,36 +203,36 @@ int main(int argc, char **argv)
                               screen.h * 0.66,
                               SDL_WINDOW_OPENGL);
     if (window == NULL)
-        end_sdl(0, "ERROR WINDOW CREATION", window, renderer, sol, ciel, mario);
+        end_sdl(0, "ERROR WINDOW CREATION", window, renderer, sol, ciel, oiseau);
 
     /* Création du renderer */
 
     renderer = SDL_CreateRenderer(window, -1,
                                   SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == NULL)
-        end_sdl(0, "ERROR RENDERER CREATION", window, renderer, sol, ciel, mario);
+        end_sdl(0, "ERROR RENDERER CREATION", window, renderer, sol, ciel, oiseau);
 
     /*Creation image sol*/
 
     sol = IMG_LoadTexture(renderer, "./img/terrain.png");
     if (sol == NULL)
-        end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer, sol, ciel, mario);
+        end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer, sol, ciel, oiseau);
 
     /*Creation image ciel*/
 
     ciel = IMG_LoadTexture(renderer, "./img/ciel.png");
     if (ciel == NULL)
-        end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer, sol, ciel, mario);
+        end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer, sol, ciel, oiseau);
 
-    /*Creation image mario*/
+    /*Creation image oiseau*/
 
-    mario = IMG_LoadTexture(renderer, "./img/frame-mario.png");
-    if (mario == NULL)
-        end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer, sol, ciel, mario);
+    oiseau = IMG_LoadTexture(renderer, "./img/oiseau.png");
+    if (oiseau == NULL)
+        end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer, sol, ciel, oiseau);
 
     /*Affichage*/
 
-    anim(ciel, window, renderer, sol, mario);
+    anim(ciel, window, renderer, sol, oiseau);
     SDL_Delay(2000);
 
     SDL_RenderClear(renderer); // Effacer la fenêtre
@@ -232,6 +241,6 @@ int main(int argc, char **argv)
 
     /* on referme proprement la SDL */
 
-    end_sdl(1, "Normal ending", window, renderer, sol, ciel, mario);
+    end_sdl(1, "Normal ending", window, renderer, sol, ciel, oiseau);
     return EXIT_SUCCESS;
 }
