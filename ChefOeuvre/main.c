670fa7488a2373dec145385884f83426b027c4f1
Xethail <xethail@xana.code_lyoko>
2022-06-22 22:53:24 +0200

    LEGER : fonction MaxScore fonctionnelle

--- ChefOeuvre/main.c
+++ ChefOeuvre/main.c
@@ -1,104 +1,54 @@
-#include <stdlib.h>
 #include <stdio.h>
-
-#define TAILLE_TABLEAU 10
-
-SDL_bool
-  program_on = SDL_TRUE,                          // Booléen pour dire que le programme doit continuer
-  paused = SDL_FALSE;                             // Booléen pour dire que le programme est en pause
-SDL_Event event;
-
-/*Création tableau*/
-
-int ** creer_tableau(int nb_lignes, int nb_colonnes)
-{
-    int ** tableau = (int**)malloc(nb_lignes * sizeof(int *));
-    int i;
-    for (i = 0; i < nb_lignes; i++)
-    {
-        tableau[i] = (int *)calloc(nb_colonnes, sizeof(int));
-    }
-    return tableau;
-}
-
-/*LIberation tableau*/
-
-void liberer_tableau(int ** tableau, int nb_lignes)
-{
-    int i;
-    for (i = 0; i < nb_lignes; i++)
-    {
-        free(tableau[i]);
-    }
-    free(tableau);
-}
+#include <stdlib.h>
 
 
+/*
+ *tableau de markov qui va contient les probabilités 
+ *de passer entre les différents états :
+ *vitesse/2 : indice 0 , 
+ *vitesse   : indice 1 , 
+ *vitesse*2 : indice 2 ,
+ *vitesse*3 : indice 3 ,
+ *endormi   : indice 4
+*/
 
-void afficher_tableau(int ** tableau, int nb_lignes, int nb_colonnes)
-{
-    int i, j;
-    for (i = 0; i < nb_lignes; i++)
-    {
-        for (j = 0; j < nb_colonnes; j++)
-        {
-            printf("%d ", tableau[i][j]);
-        }
-        printf("\n");
-    }
-}
 
-/* Création du serpent au milieu du cadre */
+float markov [5][5] = {{ 0.15,   0.5, 0.25,    0,   0.1},
+		       { 0.1 ,   0.4,  0.3, 0.15,  0.05},
+		       {0.025,  0.15,  0.5,  0.3, 0.025},
+		       {0.075, 0.075, 0.15,  0.4,   0.3},
+		       {  0.3,     0,    0,    0,   0.7}}; 
 
-void InitialisationSerpent(int ** tableau, int taille){  
-    int i = taille/2,j;
+// passer d'un état à un autre
 
-    for(j = taille/2 - 2; j<3; j++){
-        tableau[i][j] = 1;
-    }
-    tableau[i][3]=2;
 
-}
+int  meilleurScore(int ScoreActuel){
+  
+  int MeilleurScore;
+  char PremiereLigne[10];
+  
+  FILE * score = fopen("score.txt","r+");
 
-/* Création des probabilités de changement de place de la pomme */
+  MeilleurScore = atoi(fgets(PremiereLigne, 10, score));
 
-void MarkovPomme(int ** probapomme){
-    int a,b,c,d,e,f,g,h,i;
-    int j,k;
+  if (MeilleurScore < ScoreActuel){
 
-    //remplir le tableau avec les probabilités
-
+    MeilleurScore = ScoreActuel;
+    rewind(score);
+    fprintf(score,"%d",MeilleurScore);
+    
+  }
+  
+  fclose(score);
+    
+  return MeilleurScore;
 }
 
 
-int main(){
 
-    int ** cadre = creer_tableau(TAILLE_TABLEAU, TAILLE_TABLEAU);
-    int ** probapomme = creer_tableau(4,4);
-    int tailleserpent = 4
-    MarkovPomme(probapomme);
-    InitialisationSerpent(cadre, TAILLE_TABLEAU);
+int main (){  
 
-    while(program_on){
-        if(SDL_PollEvent(&event)) {   // Tant que la file des évènements stockés n'est pas vide et qu'on n'a pas terminé le programme Défiler l'élément en tête de file dans 'event'
-            switch(event.type){
-                case SDLK_UP:         //Cas flèche du haut
-
-
-                case SDLK_DOWN:         //Cas flèche du bas
-
-                case SDLK_RIGHT:        //Cas flèche droite
-
-                case SDLK_LEFT:         //Cas flèche gauche
-
-                
-
-            }
-
-    }
-
-
-
-
-    return 0;
+  printf("%d",meilleurScore(5));
+  
+  return 0;
 }
