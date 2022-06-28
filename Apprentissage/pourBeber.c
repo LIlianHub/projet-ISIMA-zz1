e9e1de79f849c05e21fafa1cf8bedd1e7baebb30
LILianHUb <lilian.ballejos@hotmail.fr>
2022-06-28 11:40:43 +0200

    etat

--- Apprentissage/pourBeber.c
+++ Apprentissage/pourBeber.c
@@ -1,4 +1,11 @@
 
+typedef struct etat{
+  int nord_sud;
+  int ouest_est;
+}etat;
+
+
+
 
 int calculEtat(int teteSx, int teteSy, int pommex, int pommey)
 {
@@ -53,4 +60,30 @@ int calculEtat(int teteSx, int teteSy, int pommex, int pommey)
     }
 
     return etat;
+}
+
+
+etat * liste_etats;
+
+void genereTableauEtat()
+{
+    liste_etats = (etat *)malloc(sizeof(etat) * 9);
+    liste_etats[0].nord_sud = -1;
+    liste_etats[0].ouest_est = -1;
+    liste_etats[1].nord_sud = -1;
+    liste_etats[1].ouest_est = 0;
+    liste_etats[2].nord_sud = -1;
+    liste_etats[2].ouest_est = 1;
+    liste_etats[3].nord_sud = 0;
+    liste_etats[3].ouest_est = -1;
+    liste_etats[4].nord_sud = 0;
+    liste_etats[4].ouest_est = 0;
+    liste_etats[5].nord_sud = 0;
+    liste_etats[5].ouest_est = 1;
+    liste_etats[6].nord_sud = 1;
+    liste_etats[6].ouest_est = -1;
+    liste_etats[7].nord_sud = 1;
+    liste_etats[7].ouest_est = 0;
+    liste_etats[8].nord_sud = 1;
+    liste_etats[8].ouest_est = 1;
 }
